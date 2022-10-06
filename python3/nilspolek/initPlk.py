771a650d4d5b2e0df43652f63d0fb404a35f7d81
nilspolek <nils@polek.de>
2022-10-06 12:22:05 +0200

    test

--- python3/nilspolek/initPlk.py
+++ python3/nilspolek/initPlk.py
@@ -1,15 +1,34 @@
-import vim
+import vim, subprocess
 
 # get file vim.eval("expand('%:p')")
 # get directory vim.eval("expand('%:p:h')")
 
 class Klasse:
 
+    def getDir(self) -> str:
+        return vim.eval("expand('%:p:h')")
+
+    def getFileDir(self) -> str:
+        return vim.eval("expand('%:p')")
+    
+    def getFileType(self) -> str:
+        return vim.eval("&filetype")
+
     def initPython(self) -> None:
         print("Es hat funktionier")
         print(vim.eval("expand('%:p')"))
         return True
     
-    def buildJava(self) -> None:
-        pass
+    def Build(self) -> None:
+        if getFileType() == "java":
+            subprocess.run(f"javac {getFileDir()}")
+            return True
+        print("Filetype not supported")
+        return True
+    
+    def Run(self) -> None:
+        if getFileType() == "java":
+            subprocess.run(f"javac {getFileDir()}")
+            subprocess.run(f"java {getDir()}")
+
         
