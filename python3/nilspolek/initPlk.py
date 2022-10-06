261b05be36ea0550c86d0a573669fdd65b55ef1e
nilspolek <nils@polek.de>
2022-10-06 12:00:23 +0200

    test

--- python3/nilspolek/initPlk.py
+++ python3/nilspolek/initPlk.py
@@ -1,10 +1,13 @@
 import vim
 
+# get file vim.eval("expand('%:p')")
+# get directory vim.eval("expand('%:p:h')")
+
 class Klasse:
 
     def initPython(self) -> None:
         print("Es hat funktionier")
-        print(vim.eval("&filetype"))
+        print(vim.eval("expand('%:p')"))
         return True
     
     def buildJava(self) -> None:
